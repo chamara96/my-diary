71824c6d41f44f7afff686567bc22879917476ca
Chamara Herath <cmb.info96@gmail.com>
2025-09-10 21:31:22 +0530

    Update env

--- budget/settings.py
+++ budget/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wb7j)%b^st(%^3vsi@m@baqt@p_0-++y%!g$r5ciy(4dy1(kti"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
@@ -120,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+SERVER_PUBLIC_FOLDER = os.environ.get("SERVER_PUBLIC_FOLDER", BASE_DIR)
+
 STATIC_URL = "static/"
+STATIC_ROOT = f"{SERVER_PUBLIC_FOLDER}/static/"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = f"{SERVER_PUBLIC_FOLDER}/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
